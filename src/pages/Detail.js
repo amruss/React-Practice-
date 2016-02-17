8d342d9fa74c8086fd3713d8462a893abe1eae7a
Abbey Russell <amruss@mit.edu>
2016-02-17 18:34:55 -0500

    added API call from NYT Congress API

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ajax from 'superagent';
 
 
+
 class Detail extends React.Component {
+    
+    
     constructor(props) {
         super(props);
         
@@ -12,9 +15,10 @@ class Detail extends React.Component {
     }
     
     componentWillMount() {
-        ajax.get('https://api.github.com/repos/facebook/react/commits')
+        ajax.get('http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/R000595/votes.json?api-key=4ac8b311959f5fbce0e348b805613ea3:9:74443113')
         .end((error, response) => {
              if (!error && response) {
+             console.dir(response.body);
              this.setState({ commits: response.body });
              } else {
              console.log('There was an error fetching from GitHub', error);
@@ -26,10 +30,9 @@ class Detail extends React.Component {
     
     
     render() {
+        console.log(this.state.commits.results);
         return (<div>
-                {this.state.commits.map((commit, index) => (
-                                                            <p key = {index}>Some commit data here.</p>
-                                                            ))};
+                <p>Hey</p>
                 </div>);
     }
 }
