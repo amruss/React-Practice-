c6e857c431f64afb43a3d0cae3a8fff34b0c4cfa
Abbey Russell <amruss@mit.edu>
2016-02-18 18:51:56 -0500

    working with rubio

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -10,11 +10,7 @@ class Detail extends React.Component {
         super(props);
         
         this.state = {
-            mode: "rubio",
             rubio: [],
-            hClinton: [],
-            cruz:[],
-            sanders: []
         };
         
         
@@ -31,44 +27,6 @@ class Detail extends React.Component {
              }
              );
         
-        /*ajax.get('http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/R000595/votes.json?api-key=4ac8b311959f5fbce0e348b805613ea3:9:74443113')
-        .end((error, response) => {
-             if (!error && response) {
-             
-             this.setState({ cruz: response.body});
-
-             } else {
-             console.log('There was an error fetching from NYT', error);
-             }
-             }
-             );
-        
-        ajax.get('http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/R000595/votes.json?api-key=4ac8b311959f5fbce0e348b805613ea3:9:74443113')
-        .end((error, response) => {
-             if (!error && response) {
-             
-             console.dir(response.body);
-             this.setState({ hClinton: response.body});
-
-             } else {
-             console.log('There was an error fetching from NYT', error);
-             }
-             }
-             );
-        
-        ajax.get('http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/R000595/votes.json?api-key=4ac8b311959f5fbce0e348b805613ea3:9:74443113')
-        .end((error, response) => {
-             if (!error && response) {
-             
-             console.dir(response.body);
-             this.setState({ sanders: response.body});
-   
-             } else {
-             console.log('There was an error fetching from NYT', error);
-             }
-             }
-             );*/
-        
         
     }
 
@@ -77,66 +35,20 @@ class Detail extends React.Component {
 
     
     
-    renderCruz() {
-        return this.state.commits.results ? this.state.rubio.results[0].votes.map((vote, index) => (
-                                                                                                       
-                                                                                                       (<p key={index}>
-                                                                                                        <strong>{vote.position}: </strong>
-                                                                                                        <p2>{vote.description}</p2>
-                                                                                                        </p>))) :
-            this.state.commits.map((vote, index) => ( <p>NOPE</p>))
-    }
-    renderRubio(){
-        return this.state.commits.results ? this.state.rubio.results[0].votes.map((vote, index) => (
-                                                                                                
-                                                                                                (<p key={index}>
-                                                                                                 <strong>{vote.position}: </strong>
-                                                                                                 <p2>{vote.description}</p2>
-                                                                                                 </p>))) :
-            this.state.commits.map((vote, index) => ( <p>NOPE</p>))
-        
-    }
-    renderClinton(){
-        return this.state.commits.results ? this.state.rubio.results[0].votes.map((vote, index) => (
-                                                                                                
-                                                                                                (<p key={index}>
-                                                                                                 <strong>{vote.position}: </strong>
-                                                                                                 <p2>{vote.description}</p2>
-                                                                                                 </p>))) :
-            this.state.commits.map((vote, index) => ( <p>NOPE</p>))
-        
-    }
-    renderSanders(){
-        return this.state.commits.results ? this.state.rubio.results[0].votes.map((vote, index) => (
-                                                                                                
-                                                                                                (<p key={index}>
-                                                                                                 <strong>{vote.position}: </strong>
-                                                                                                 <p2>{vote.description}</p2>
-                                                                                                 </p>))) :
-            this.state.commits.map((vote, index) => ( <p>NOPE</p>))
-        
-    }
-
+    
     
     
     render() {
-        let content;
-        
-        if(this.state.mode === "cruz"){
-            content = this.renderCruz();
-        } else if (this.state.mode === "hClinton"){
-            content = this.renderClinton();
-        } else if (this.state.mode == "rubio"){
-            content = this.renderRubio();
-        } else {
-            content = this.renderSanders();
-        }
         
         return (<div>
-                <button onClick = {this.rubioClicked.bind(this)}>Marco Rubio</button>
-                <button onClick = {this.clintonClicked.bind(this)}>Hilary Clinton</button>
-                <button onClick = {this.cruzClicked.bind(this)}>Ted Cruz</button>
-                <button onClick = {this.sandersClicked.bind(this)}>Sanders</button>
+                {this.state.rubio.results ? this.state.rubio.results[0].votes.map((vote, index) => (
+                                                                                                            
+                                                                                                            (<p key={index}>
+                                                                                                             <strong>{vote.position}: </strong>
+                                                                                                             <p2>{vote.description}</p2>
+                                                                                                             </p>))) :
+                this.state.rubio.map((vote, index) => ( <p>NOPE</p>))}
+
                 
                 </div>);
     }
