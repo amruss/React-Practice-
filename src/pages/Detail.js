86cc988afac7dec0838f69710fd246ab713f0ad2
Abbey Russell <amruss@mit.edu>
2016-02-22 14:47:16 -0500

    trying to move to inout congressman id, pretty buggy atm

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -5,66 +5,84 @@ import ajax from 'superagent';
 
 class Detail extends React.Component {
     
-    
+
+
     constructor(props) {
         super(props);
+    
         
         this.state = {
-            rubio: [],
+        value: 'R000595',
+        mode: "rubio",
+        congressMan: [],
         };
         
         
+        
     }
+
+    fetchFeed(congressID) {
+        ajax.get('http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/' + congressID + '/votes.json?api-key=4ac8b311959f5fbce0e348b805613ea3:9:74443113')
+                 .end((error, response) => {
+                      if (!error && response) {
+                      this.setState({ congressMan : response.body});
+                      } else {
+                      console.log("There was an error fetching from NYT", error);
+                      }
+                      }
+                      );
+                 }
     
     componentWillMount() {
-        ajax.get('http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/R000595/votes.json?api-key=4ac8b311959f5fbce0e348b805613ea3:9:74443113')
-        .end((error, response) => {
-             if (!error && response) {
-             this.setState({ rubio: response.body});
-             } else {
-             console.log('There was an error fetching from NYT', error);
-             }
-             }
-             );
-        
+        this.fetchFeed(this.state.value);
+        /*this.fetchFeed("hClinton", 'C001041');
+        this.fetchFeed("cruz", 'C001098');
+        this.fetchFeed("sanders", 'S000033');*/
         
     }
 
-
-    
-
-    
-    
-    
-    
-    
-    render() {
+    renderRubio() {
         
         return (<div>
-                {this.state.rubio.results ? this.state.rubio.results[0].votes.map((vote, index) => (
-                                                                                                            
-                                                                                                            (<p key={index}>
-                                                                                                             <strong>{vote.position}: </strong>
-                                                                                                             <p2>{vote.description}</p2>
-                                                                                                             </p>))) :
-                this.state.rubio.map((vote, index) => ( <p>NOPE</p>))}
-
+                {this.state.congressMan.results ? this.state.congressMan.results[0].votes.map((vote, index) => (
+                                                                                                    
+                                                                                                    (<p key={index}>
+                                                                                                     <strong>{vote.position}: </strong>
+                                                                                                     <p2>{vote.description}</p2>
+                                                                                                     </p>))) :
+                this.state.congressMan.map((vote, index) => ( <p>NOPE</p>))}
+                
                 
                 </div>);
     }
-    rubioClicked() {
-        this.setState({mode: "rubio"});
+
+    
+
+    
+    render() {
+        let content;
+        
+        
+        content = this.renderRubio();
+        
+        
+        return (<div>
+                <input value={this.state.value} onChange={this.handleChange.bind(this)} />
+                <button onClick = {this.selectMode.bind(this, this.state.value)}>Marco Rubio</button>
+
+                {content}
+                </div>
+                );
+
     }
-    clintonClicked() {
-        this.setState({mode: "hClinton"});
+    
+    handleChange(name){
+        this.setState({ value: name.target.value})
     }
-    cruzClicked() {
-        this.setState({mode: "cruz"});
-    }
-    sandersClicked(){
-        this.setState({mode: "sanders"});
-    }
+
+    
 }
+
 
 
 export default Detail;
